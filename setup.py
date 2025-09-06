9a60c7845c9e427352f1da783ada856bb7f05e6b
Kush Dutta <kushdutta717@gmail.com>
2025-09-06 07:41:25 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_namespace_packages, setup
+
+setup(
+    name= 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Kush Dutta',
+    author_email= 'kushdutta717@gmail.com',
+    packages= find_namespace_packages(),
+    install_required = []
+)
